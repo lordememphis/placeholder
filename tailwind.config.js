11f26fc6a79ecf986ab084935cf1de620b420332
Tafadzwa Muteke <muteket@gmail.com>
2019-10-30 10:17:07 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#e6e8e9",
+          "100": "#ced2d3",
+          "200": "#b5bbbd",
+          "300": "#9ca4a7",
+          "400": "#848e92",
+          "500": "#6b777c",
+          "600": "#526066",
+          "700": "#394950",
+          "800": "#21333a",
+          "900": "#081c24"
+        },
+        accent: {
+          "50": "#e6fbf1",
+          "100": " #ccf6e4",
+          "200": " #b3f2d6",
+          "300": " #99edc9",
+          "400": " #80e9bb",
+          "500": " #67e4ad",
+          "600": " #4de0a0",
+          "700": " #34db92",
+          "800": " #1ad785",
+          "900": " #01d277"
+        }
+      }
+    },
+    fontFamily: {
+      display: ["Montserrat", "helvetica", "sans-serif"],
+      body: ["Montserrat", "helvetica", "sans-serif"]
+    }
+  },
+  variants: {},
+  plugins: []
+};
