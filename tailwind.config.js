62daeaccac3c5f0216865a0e0e3a438a8096d4fb
Tafadzwa Muteke <muteket@gmail.com>
2020-08-29 14:47:40 +0200

    Declare custome breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,6 +84,11 @@ module.exports = {
     fontFamily: {
       inter: ["Inter", "sans-serif"],
     },
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
   },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "group-hover"],
