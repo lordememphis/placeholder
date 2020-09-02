35992dd9676c3ea2a514d9152212c491d74399c1
Tafadzwa Muteke <muteket@gmail.com>
2020-09-02 04:35:48 +0200

    Remove custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,11 +84,6 @@ module.exports = {
     fontFamily: {
       inter: ["Inter", "sans-serif"],
     },
-    screens: {
-      tablet: "640px",
-      laptop: "1024px",
-      desktop: "1280px",
-    },
   },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "group-hover"],
